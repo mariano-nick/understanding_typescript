0a06496023db51adaaf92f35ee721eb46819263d
nmariano <mariano.nick@gmail.com>
2019-02-21 22:15:04 -0600

    Through Section 2 - Using Types

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 console.log("It works!");
 // Assigning Types
 // if you don't assign a value at init, TS sees var as type = any
-var myRealAge;
-myRealAge = 'Nick';
-myRealAge = 27; // this compiles
+var myRealAgeName;
+myRealAgeName = 'Sam';
+myRealAgeName = 62; // this compiles
 // we can be explicit about a type using : <type>
 var mySuperRealAge;
 mySuperRealAge = 27;
@@ -13,5 +13,100 @@ mySuperRealAge = 27;
 var hobbies = ['Cooking', 'Sports'];
 console.log(hobbies[0]); // Cooking
 // tuples
-// arrays with mixed types and limited number of items
+// arrays with mixed types
 var address = ['Main Street', 99];
+// enums
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+    Color[Color["Yellow"] = 3] = "Yellow";
+})(Color || (Color = {}));
+var myColor = Color.Gray;
+console.log(myColor); // 0
+// var string helloString = "hello";
+// let helloString: string = "hello";
+// any, should be used sparingly, when extreme flexibility is needed
+var car = "BMW";
+car = { brand: "BMW", series: 3 };
+// functions
+function returnMyName() {
+    return myRealAgeName;
+}
+console.log(returnMyName());
+// void functions
+function sayHello() {
+    console.log("Hello!");
+}
+sayHello();
+// functions with typed arguments
+function multiply(firstValue, secondValue) {
+    return firstValue * secondValue;
+}
+console.log(multiply(2, "Max")); // NaN
+// function types
+var myMultiply;
+myMultiply = sayHello;
+myMultiply(); // Hello!
+myMultiply = multiply;
+console.log(myMultiply(5, 2)); // 10
+var myMultiplyWithArguments; // works like a delegate definition
+// let myFunction: () => number     // defines a function that takes no parameters and returns a number
+var voidFunction;
+myMultiplyWithArguments = multiply;
+// myMultiplyWithArguments = sayHello;      // compile error
+// objects
+var user = {
+    name: "Jim",
+    age: 45
+};
+user = {
+    name: "Hello",
+    age: 22
+};
+// complex object
+// var complex, which has two properties, data is a number array,
+// output is a function, that takes a single boolean argument, and returns a number array
+// { object data }
+var complex = {
+    data: [100, 2.2, 10],
+    output: function (isHappy) {
+        if (isHappy) {
+            return this.data;
+        }
+        return [1];
+    }
+};
+console.log(complex.output(true));
+console.log(complex.output(false));
+var complex2 = {
+    data: [100, 2.2, 10],
+    output: function (isHappy) {
+        if (isHappy) {
+            return this.data;
+        }
+        return [1];
+    }
+};
+// sometimes you want to define a property that can be of only a certain few types - data is number array or string array
+// union types
+var myData = [1]; // myData is of type number array OR string array
+myData = ["Hello"];
+// checking types
+var finalValue = "A string";
+if (typeof finalValue == "string") {
+    console.log(finalValue);
+}
+if (typeof finalValue == "number") {
+    console.log(finalValue + 1);
+}
+// never (rarely used)
+function neverReturns() {
+    throw new Error("An error!");
+}
+// nullable types
+var canBeNull = 12;
+canBeNull = null;
+var canAlsoBeNull;
+canAlsoBeNull = null;
--- app.ts
+++ app.ts
@@ -3,9 +3,9 @@ console.log("It works!");
 // Assigning Types
 
 // if you don't assign a value at init, TS sees var as type = any
-let myRealAge: any;
-myRealAge = 'Nick';
-myRealAge = 27;    // this compiles
+let myRealAgeName: any;
+myRealAgeName = 'Sam';
+myRealAgeName = 62;    // this compiles
 
 // we can be explicit about a type using : <type>
 let mySuperRealAge: number;
@@ -17,6 +17,133 @@ let hobbies: string[] = ['Cooking', 'Sports'];
 console.log(hobbies[0]);    // Cooking
 
 // tuples
-// arrays with mixed types and limited number of items
+// arrays with mixed types
 let address: [string, number] = ['Main Street', 99];
+
+// enums
+enum Color{
+    Gray,
+    Green,
+    Blue,
+    Yellow
+}
+let myColor: Color = Color.Gray;
+console.log(myColor);   // 0
+
+// var string helloString = "hello";
+// let helloString: string = "hello";
+
+// any, should be used sparingly, when extreme flexibility is needed
+let car: any = "BMW";
+car = { brand: "BMW", series: 3};
+
+// functions
+function returnMyName(): string {   // type declaration after function() defines return type (string)
+    return myRealAgeName;
+}
+
+console.log(returnMyName());
+
+// void functions
+function sayHello(): void {
+    console.log("Hello!");
+}
+
+sayHello();
+
+// functions with typed arguments
+function multiply(firstValue: number, secondValue: any): number {
+    return firstValue * secondValue;
+}
+
+console.log(multiply(2, "Max"));    // NaN
+
+
+// function types
+let myMultiply;
+myMultiply = sayHello;
+myMultiply();   // Hello!
+myMultiply = multiply;
+console.log(myMultiply(5 , 2)); // 10
+
+let myMultiplyWithArguments: (arg1: number, arg2: number) => number;    // works like a delegate definition
+    // let myFunction: () => number     // defines a function that takes no parameters and returns a number
+
+let voidFunction: (arg1: boolean) => void;
+
+myMultiplyWithArguments = multiply;
+    // myMultiplyWithArguments = sayHello;      // compile error
+
+
+// objects
+let user: { name: string, age: number } = {     // object: {propertyName: propertyType, ...} is blueprint for object
+    name: "Jim",
+    age: 45
+};
+user = {
+    name: "Hello",
+    age: 22
+}
+
+
+// complex object
+// var complex, which has two properties, data is a number array,
+// output is a function, that takes a single boolean argument, and returns a number array
+// { object data }
+let complex: {data: number[], output: (arg1: boolean) => number[]} = {
+    data: [100, 2.2, 10],
+    output: function(isHappy: boolean): number[] {
+        if (isHappy) {
+            return this.data;
+        }
+        return [1];
+    }
+};
+
+console.log(complex.output(true));
+console.log(complex.output(false));
+
+
+// complex objects built on the fly like above is not sustainable, can use classes
+// can also use a 'type alias'
+type ComplexObject = {data: number[], output: (arg1: boolean) => number[]};
+
+let complex2: ComplexObject = {
+    data: [100, 2.2, 10],
+    output: function(isHappy: boolean): number[] {
+        if (isHappy) {
+            return this.data;
+        }
+        return [1];
+    }
+};
+
+
+// sometimes you want to define a property that can be of only a certain few types - data is number array or string array
+// union types
+let myData: number[] | string[] = [1];  // myData is of type number array OR string array
+myData = ["Hello"];
+
+// checking types
+let finalValue = "A string";
+
+if (typeof finalValue == "string"){
+    console.log(finalValue);
+}
+
+if (typeof finalValue == "number"){
+    console.log(finalValue + 1);
+}
+
+
+// never (rarely used)
+function neverReturns(): never {    // never returns anything, makes your intention clear
+    throw new Error("An error!");
+}
+
+// nullable types
+let canBeNull: number | null = 12;
+canBeNull = null;
+let canAlsoBeNull;
+canAlsoBeNull = null;
 
